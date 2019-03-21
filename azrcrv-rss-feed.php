c62008cc8002c06647259e26a3de94ff7967bffe
azurecurve <development@azurecurve.co.uk>
2019-03-21 18:56:40 +0000

    v1.0.0

---
+++ azrcrv-rss-feed.php
@@ -0,0 +1,141 @@
+<?php
+/**
+ * ------------------------------------------------------------------------------
+ * Plugin Name: RSS Feed
+ * Description: Provides opposite rss feed to that configured in ClassicPress
+ * Version: 1.0.0
+ * Author: azurecurve
+ * Author URI: https://development.azurecurve.co.uk/classicpress-plugins/
+ * Plugin URI: https://development.azurecurve.co.uk/classicpress-plugins/rss-feed
+ * Text Domain: rss-feed
+ * Domain Path: /languages
+ * ------------------------------------------------------------------------------
+ * This is free software released under the terms of the General Public License,
+ * version 2, or later. It is distributed WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Full
+ * text of the license is available at https://www.gnu.org/licenses/gpl-2.0.html.
+ * ------------------------------------------------------------------------------
+ */
+
+// include plugin menu
+require_once(dirname( __FILE__).'/pluginmenu/menu.php');
+
+// Prevent direct access.
+if (!defined('ABSPATH')){
+	die();
+}
+
+/**
+ * Setup actions and filters.
+ *
+ * @since 1.0.0
+ *
+ */
+// add actions
+add_action('init', 'azrcrv_rssf_init_rss_feed');
+add_action('admin_menu', 'azrcrv_rssf_create_admin_menu');
+
+// add filters
+add_filter('plugin_action_links', 'azrcrv_rssf_add_plugin_action_link', 10, 2);
+
+/**
+ * initialise rss feed
+ *
+ * @since 1.0.0
+ *
+ */
+function azrcrv_rssf_init_rss_feed(){
+	
+	if (get_option('rss_use_excerpt')){
+		$rss_type = 'detail';
+	}else{
+		$rss_type = 'summary';
+	}
+	add_feed($rss_type, 'create_rss_feed');
+	
+	//Ensure the $wp_rewrite global is loaded
+	global $wp_rewrite;
+	//Call flush_rules() as a method of the $wp_rewrite object
+	$wp_rewrite->flush_rules(false);
+}
+
+/**
+ * Create alternate rss feed
+ *
+ * @since 1.0.0
+ *
+ */
+function azrcrv_rssf_create_rss_feed(){
+	load_template(plugin_dir_path(__FILE__).'templates/rss_feed2.php');
+}
+
+/**
+ * Add RSS Feed action link on plugins page.
+ *
+ * @since 1.0.0
+ *
+ */
+function azrcrv_rssf_add_plugin_action_link($links, $file){
+	static $this_plugin;
+
+	if (!$this_plugin){
+		$this_plugin = plugin_basename(__FILE__);
+	}
+
+	if ($file == $this_plugin){
+		$settings_link = '<a href="'.get_bloginfo('wpurl').'/wp-admin/admin.php?page=azrcrv-rssf">'.esc_html__('Settings' ,'rss-feed').'</a>';
+		array_unshift($links, $settings_link);
+	}
+
+	return $links;
+}
+
+/**
+ * Add RSS Feed menu to plugin menu.
+ *
+ * @since 1.0.0
+ *
+ */
+function azrcrv_rssf_create_admin_menu(){
+	//global $admin_page_hooks;
+	
+	add_submenu_page("azrcrv-plugin-menu"
+						,esc_html__("RSS Feed Settings", "rss-feed")
+						,esc_html__("RSS Feed", "rss-feed")
+						,'manage_options'
+						,'azrcrv-rssf'
+						,'azrcrv_rssf_settings');
+}
+
+/**
+ * Display Settings page.
+ *
+ * @since 1.0.0
+ *
+ */
+function azrcrv_rssf_settings(){
+	if (!current_user_can('manage_options')){
+		$error = new WP_Error('not_found', esc_html__('You do not have sufficient permissions to access this page.' , 'rss-feed'), array('response' => '200'));
+		if(is_wp_error($error)){
+			wp_die($error, '', $error->get_error_data());
+		}
+	}
+	?>
+	<div id="azrcrv-rssf-general" class="wrap">
+		<h2><?php echo esc_html(get_admin_page_title()); ?></h2>
+
+		<?php esc_html_e('<p>This plugin provides opposite rss feed to that configured in WordPress; e.g. if WordPress is configured for summary then an alternative feed called detail will be created, or if WordPress is configured for a detailed feed then an alternative feed called summary is created.</p>
+
+		<p>Once active, both summary and detail feeds cab be access using the following paths:', 'rss-feed');
+
+		if (get_option('rss_use_excerpt')){
+			echo '<ul><li><li><a href="'.esc_url(site_url()).'/feed">'.esc_url(site_url()).'/feed</a></li><a href="'.esc_url(site_url()).'/feed/detail">'.esc_url(site_url()).'/feed/detail</a></li></ul></p>';
+		}else{
+			echo '<ul><li><a href="'.esc_url(site_url()).'/feed/summary">'.esc_url(site_url()).'/feed/summary</a></li><li><a href="'.esc_url(site_url()).'/feed">'.esc_url(site_url()).'/feed</a></li></ul></p>';
+		}
+	?>
+	</div>
+	<?php
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,36 @@
+=== RSS Feed ===
+Contributors: azurecurve
+Tags: rss, feed
+Donate link: https://development.azurecurve.co.uk/support-development/
+Requires at least: 1.0.0
+Tested up to: 1.0.0
+Requires PHP: 5.6
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Provides opposite rss feed to that configured in ClassicPress; e.g. if ClassicPress is configured for summary then an alternative feed called detail will be created.
+
+== Description ==
+Provides opposite rss feed to that configured in ClassicPress; e.g. if ClassicPress is configured for summary then an alternative feed called detail will be created, or if ClassicPress is configured for a detailed feed then an alternative feed called summary is created.
+
+Once active, both summary and detail feeds cab be access using the following paths:
+* http://yourdomain/feed/detail
+* http://yourdomain/feed/summary
+
+== Installation ==
+To install the RSS Feed plugin:
+* Download the plugin from <a href='https://github.com/azurecurve/azrcrv-post-archive/'>GitHub</a>.
+* Upload the entire zip file using the Plugins upload function in your ClassicPress admin panel.
+* Activate the plugin.
+
+== Changelog ==
+Changes and feature additions for the Icons plugin:
+= 1.0.0 =
+* First version for ClassicPress forked from azurecurve RSS Feed WordPress Plugin.
+
+== Frequently Asked Questions ==
+= Can I translate this plugin? =
+* Yes, the .pot fie is in the plugin's languages folder and can also be downloaded from the plugin page on https://development.azurecurve.co.uk; if you do translate this plugin please sent the .po and .mo files to translations@azurecurve.co.uk for inclusion in the next version (full credit will be given).
+= Is this plugin compatible with both WordPress and ClassicPress? =
+* This plugin is developed for ClassicPress, but will likely work on WordPress.
